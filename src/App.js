686944504e17dfe131b5a300c6b8e00997db32a3
Priya Aswani <priya.aswani28139@gmail.com>
2021-05-07 12:21:07 +0400

    Create toDos state variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
 const App = () => {
+  const [toDos, setToDos] = useState([]);
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Form toDos={toDos} setToDos={setToDos} />
     </div>
   );
 };

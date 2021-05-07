5b05d72b51efc3ce6b0841abd95d7bfb627203cc
Priya Aswani <priya.aswani28139@gmail.com>
2021-05-07 14:47:14 +0400

    Render tasks using ToDo component in ToDoList

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ToDo from "./ToDo";
 
-const ToDoList = () => {
+const ToDoList = ({ toDos }) => {
+  const [active, setActive] = useState(0);
+  useEffect(() => {
+    let count = 0;
+    toDos.map((toDo) => {
+      if (toDo.status === "Active") {
+        count++;
+      }
+    });
+    setActive(count);
+  }, [toDos]);
   return (
     <>
-      <ul className="todos"></ul>
+      <ul className="todos">
+        {toDos.map((toDo, index) => (
+          <ToDo toDo={toDo} key={index} />
+        ))}
+      </ul>
       <div className="card stat">
         <p className="corner">
-          <span id="items-left">0</span> items left
+          <span id="items-left">{active}</span> items left
         </p>
         <div className="filter">
           <button id="all" className="on">

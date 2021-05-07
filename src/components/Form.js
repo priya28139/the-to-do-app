86767e837147635f11a670022ba87f1d03b245da
Priya Aswani <priya.aswani28139@gmail.com>
2021-05-07 12:25:48 +0400

    Add toDo to list of toDos on tap of + button in Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Form = () => {
+const Form = ({ toDos, setToDos }) => {
+  const [toDo, setToDo] = useState("");
+  const addToDo = () => {
+    if (toDo) {
+      setToDos([...toDos, toDo]);
+      setToDo("");
+    }
+  };
+
+  useEffect(() => {
+    console.log("toDos changed!", toDos);
+  }, [toDos]);
+
   return (
     <main>
       <div className="card add">
         <div className="cb-container">
-          <button id="add-btn">+</button>
+          <button id="add-btn" onClick={addToDo}>
+            +
+          </button>
         </div>
         <div className="txt-container">
-          <label for="addt">Create todo</label>
+          <label htmlFor="addt">Create todo</label>
           <input
             type="text"
             className="txt-input"
             placeholder="Create a new todo..."
-            spellcheck="false"
-            autocomplete="off"
+            spellCheck="false"
+            autoComplete="off"
             id="addt"
+            value={toDo}
+            onChange={(e) => setToDo(e.target.value)}
           />
         </div>
       </div>

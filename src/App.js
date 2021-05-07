19764e504f702e4a0b7afdf09f914f9e20ebb4bf
Priya Aswani <priya.aswani28139@gmail.com>
2021-05-07 12:26:02 +0400

    Render the Form component in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
-
+import Form from "./components/Form";
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Form />
     </div>
   );
 };
